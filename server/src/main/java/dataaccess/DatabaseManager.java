deb3fd989c5a3cbf6f5b400cb3866f4234ffca9c
ellak <ekinney0@byu.edu>
2026-03-09 21:33:01 -0600

    added a mysql dataaccess file to help create tables

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -28,7 +28,6 @@ public class DatabaseManager {
             throw new DataAccessException("failed to create database", ex);
         }
     }
-
     /**
      * Create a connection to the database and sets the catalog based upon the
      * properties specified in db.properties. Connections to the database should
