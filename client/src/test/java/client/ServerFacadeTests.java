6a7f2de1daad206ed88acd9365b94e19694d39f5
ellak <ekinney0@byu.edu>
2026-03-25 21:44:19 -0600

    wrote some unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,23 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +25,41 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @AfterEach
+    void clear() throws ResponseException {facade.clear();}
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void register() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
     }
+
+    @Test
+    void registerFail() throws Exception{
+        facade.register("player1", "password", "p1@email.com");
+        assertThrows(ResponseException.class, () -> facade.register("player1", "password", "p1@email.com"));
+    }
+
+    @Test
+    void login() throws Exception{
+        facade.register("player1", "password", "p1@email.com");
+        var authData = facade.login("player1", "password");
+        assertNotNull(authData);
+    }
+
+    @Test
+    void loginFail() throws Exception{
+        facade.register("player1", "password", "p1@email.com");
+        assertThrows(ResponseException.class, () -> facade.login("player1", "123"));
+    }
+
+    @Test
+    void logout() throws Exception{
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertDoesNotThrow(() -> facade.logout(authData.authToken()));
+    }
+
+
+
 
 }
